d7620d3308c500038f8b3947a9b1eed9d5667844
hrigitano <henrique.rigitano@ibcp.com>
2025-07-24 10:29:49 +0200

    first visualization worked

--- myfunctions.py
+++ myfunctions.py
@@ -1,5 +1,7 @@
 import pyperclip
 import cleanpipe as cl
+import os
+from pathlib import Path
 
 try:
     pyperclip.set_clipboard("xclip")
@@ -1112,13 +1114,32 @@ def call_open_vmd_with_socket():
 
 
 
-def clipboard_xxxxx():
-    """This function sends 'xxxxxxx' to the clipboard."""
+def send_tcl_to_VMD_default_cartoon_and_licorice():
+    """This function sends a tcl script to a VMD with open socket"""
     
-    # Copy text to the clipboard
-    pyperclip.copy("xxxxxxx")
 
-    # Retrieve text from the clipboard
-    sent = pyperclip.paste()
+    script_name = "default_cartoon_and_licorice.tcl"
 
-    return sent + " sent to clipboard"
+    #get where cleanpipe is, because the scripts are in a folder there
+    module_path = Path(cl.__file__)
+    module_dir = module_path.parent
+
+    #cuild the script full path
+    script_path = module_dir / "tcl" / script_name
+
+    # Convert path to a string with OS-specific formatting (safe for both Windows and Linux)
+    script_path_str = str(script_path)
+
+    # Optionally, on Windows, you can convert to POSIX-style (slashes) if needed:
+    # VMD accepts both styles but prefers forward slashes
+    script_path_str = script_path.as_posix()
+
+    #final command
+    final_command = f'source "{script_path_str}"'
+
+    # Send to VMD
+    cl.send_command_to_vmd(final_command)
+
+
+
+    return f'command "{final_command}" sent to VMD'
