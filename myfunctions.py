f228e9b8937d0f2e96d8c72a878369f586af69ff
hrigitano <henrique.rigitano@ibcp.com>
2025-09-08 13:15:28 +0200

    much more martini visualisations

--- myfunctions.py
+++ myfunctions.py
@@ -1545,6 +1545,78 @@ def send_tcl_to_VMD_MARTINI_bylipidtype():
 
     return f'command "{final_command}" sent to VMD'
 
+
+def send_tcl_to_VMD_MARTINI_bylipid_blue_red():
+    """This function sends a tcl script to a VMD with open socket"""
+
+    import cleanpipe as cl
+    
+    script_name = "MARTINI_bylipid_blue_red.tcl"
+
+    #get where cleanpipe is, because the scripts are in a folder there
+    module_path = Path(cl.__file__)
+    module_dir = module_path.parent
+    #cuild the script full path
+    script_path = module_dir / "tcl" / script_name
+    # Convert path to a string with OS-specific formatting (safe for both Windows and Linux)
+    script_path_str = str(script_path)
+    # convert to POSIX-style (slashes). good for windows. VMD accepts both styles but prefers forward slashes
+    script_path_str = script_path.as_posix()
+
+    # Send to VMD
+    final_command = f'source "{script_path_str}"'
+    cl.send_command_to_vmd(final_command)
+
+    return f'command "{final_command}" sent to VMD'
+
+def send_tcl_to_VMD_MARTINI_bylipid_bighead_smallhead():
+    """This function sends a tcl script to a VMD with open socket"""
+
+    import cleanpipe as cl
+    
+    script_name = "MARTINI_bylipid_bighead_smallhead.tcl"
+
+    #get where cleanpipe is, because the scripts are in a folder there
+    module_path = Path(cl.__file__)
+    module_dir = module_path.parent
+    #cuild the script full path
+    script_path = module_dir / "tcl" / script_name
+    # Convert path to a string with OS-specific formatting (safe for both Windows and Linux)
+    script_path_str = str(script_path)
+    # convert to POSIX-style (slashes). good for windows. VMD accepts both styles but prefers forward slashes
+    script_path_str = script_path.as_posix()
+
+    # Send to VMD
+    final_command = f'source "{script_path_str}"'
+    cl.send_command_to_vmd(final_command)
+
+    return f'command "{final_command}" sent to VMD'
+
+
+def send_tcl_to_VMD_MARTINI_bylipid_solid_liquid():
+    """This function sends a tcl script to a VMD with open socket"""
+
+    import cleanpipe as cl
+    
+    script_name = "MARTINI_bylipid_solid_liquid.tcl"
+
+    #get where cleanpipe is, because the scripts are in a folder there
+    module_path = Path(cl.__file__)
+    module_dir = module_path.parent
+    #cuild the script full path
+    script_path = module_dir / "tcl" / script_name
+    # Convert path to a string with OS-specific formatting (safe for both Windows and Linux)
+    script_path_str = str(script_path)
+    # convert to POSIX-style (slashes). good for windows. VMD accepts both styles but prefers forward slashes
+    script_path_str = script_path.as_posix()
+
+    # Send to VMD
+    final_command = f'source "{script_path_str}"'
+    cl.send_command_to_vmd(final_command)
+
+    return f'command "{final_command}" sent to VMD'
+
+
 def send_tcl_to_VMD_MARTINI_technical():
     """This function sends a tcl script to a VMD with open socket"""
 
